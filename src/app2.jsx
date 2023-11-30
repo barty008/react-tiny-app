9d4764f7afa59a28a380f823c5433e3b5755c880
barty008 <jamesb88@hotmail.co.uk>
2023-11-30 19:20:12 +0000

    starting task 2

---
+++ src/app2.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+function AppTwo() {
+  return (
+    <div>
+      <h1>Task 2:</h1>
+      <div className="container"></div>
+    </div>
+  )
+}
+export default AppTwo
